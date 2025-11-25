351d345815768fb9a27909bd8c38cc619efba1e1
EllieOS <94532192+Eliliyah@users.noreply.github.com>
2025-11-25 15:43:51 -0500

    Update zap.sh

--- zap.sh
+++ zap.sh
@@ -29,7 +29,7 @@ sgdisk --zap-all /dev/nvme0n1
 
 sgdisk --clear \
          --new=1:0:+2048MiB --typecode=1:ef00 \
-         --new=2:0:+12000MiB   --typecode=2:8200 \
+         --new=2:0:+12288MiB   --typecode=2:8200 \
          --new=3:0:0       --typecode=3:8300 \
            /dev/nvme0n1
 
