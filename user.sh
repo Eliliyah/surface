42e1e37d80b320af1b214765a1c90800098a89ab
EllieOS <94532192+Eliliyah@users.noreply.github.com>
2025-12-05 16:47:30 -0500

    Update user.sh

--- user.sh
+++ user.sh
@@ -35,7 +35,7 @@ LC_NUMERIC=en_US.UTF-8
 LANGUAGE=en_US.UTF-8">> /etc/locale.conf
 echo "KEYMAP=us
 FONT=Lat2-Terminus16">> /etc/vconsole.conf
-echo "angel">> /etc/hostname
+echo "ballerina">> /etc/hostname
 
 #Set the root password
 passwd
