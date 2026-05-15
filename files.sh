bd40803c6d718a81fbc6143fc477cc082f9ce34c
EllieOS <94532192+Eliliyah@users.noreply.github.com>
2026-05-15 11:14:24 -0400

    Update files.sh

--- files.sh
+++ files.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 #FUNCTIONS GO HERE
 
@@ -18,23 +18,26 @@ example-function() {
 
 #sync home files
 pacman -S --needed rsync starship fish --noconfirm
-rsync -av /surface/files/.bash_aliases /home/ellie/
-rsync -av /surface/files/bash_aliases /home/ellie/
-rsync -av /surface/files/bashrc /home/ellie/
-rsync -av /surface/files/fish_aliases /home/ellie
-rsync -av /surface/files/update.sh /home/ellie
-rsync -av /surface/files/starship.toml /home/ellie/.config/
+cd /home/ellie/
+git clone https://github.com/eliliyah/surface
+cd /home/ellie/home/ellie/surface/
+
+mkdir /home/ellie/.config
+rsync -av /home/ellie/surface/files/.bash_aliases /home/ellie/.bash_aliases
+rsync -av /home/ellie/surface/files/bash_aliases /home/ellie/bash_aliases
+rsync -av /home/ellie/surface/files/bashrc /home/ellie/bashrc
+rsync -av /home/ellie/surface/files/fish_aliases /home/ellie/fish_aliases
+rsync -av /home/ellie/surface/files/update.sh /home/ellie/update.sh
+rsync -av /home/ellie/surface/files/starship.toml /home/ellie/.config/starship.toml
 mkdir /home/ellie/.config/fish/
-rsync -av /surface/files/config.fish /home/ellie/.config/fish/
-rsync -av /surface/files/fish_variables /home/ellie/.config/fish
+rsync -av /home/ellie/surface/files/config.fish /home/ellie/.config/fish/config.fish
+rsync -av /home/ellie/surface/files/fish_variables /home/ellie/.config/fish/fish_variables
 mkdir /home/ellie/.config/fish/conf.d/
-rsync -av /surface/files/uv.env.fish /home/ellie/.config/fish/conf.d/uv.env.fish
+rsync -av /home/ellie/surface/files/uv.env.fish /home/ellie/.config/fish/conf.d/uv.env.fish
+mkdir /home/llie/.local/
 mkdir /home/ellie/.local/bin
-rsync -av /surface/files/env.fish /home/ellie/.local/bin/
+rsync -av /home/ellie/surface/files/env.fish /home/ellie/.local/bin/env.fish
 mkdir /home/ellie/Pictures
-rsync -av /surface/files/arch_pink_background.png /home/ellie/Pictures
-rsync -av /surface/files/ellieossticker_small.png /home/ellie/Pictures
-rsync -av /surface/files/home_files.zip/ /home/ellie/Downloads
-mkdir /home/ellie/surface/
-rsync -av /surface/ /home/ellie/surface/
-chown -R ellie:ellie /home/ellie
+rsync -av /home/ellie/surface/files/arch_pink_background.png /home/ellie/Pictures/arch_pink_background.png
+rsync -av /home/ellie/surface/files/ellieossticker_small.png /home/ellie/Pictures/ellieossticker_small.png
+rsync -av /home/ellie/surface/files/home_files.zip/ /home/ellie/Downloads/home_files.zip
