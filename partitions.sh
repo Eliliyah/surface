30f06cce5a27f88046df1f5e8d58a545a48f8fa7
EllieOS <94532192+Eliliyah@users.noreply.github.com>
2026-05-15 11:33:42 -0400

    Update partitions.sh

--- partitions.sh
+++ partitions.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 #FUNCTIONS GO HERE
 
@@ -16,11 +16,15 @@ example-function() {
     echo "$2"
 }
 
+#Choose the drive for installation
+lsblk
+read -p "Which device will you be partitioning?" dev
+confirm "Is "$dev" correct?"
 
 #Partition the drive 
-mkfs.fat -F 32 -n EFI /dev/nvme0n1p1
-mkswap -L swap -f /dev/nvme0n1p2
-mkfs.btrfs /dev/nvme0n1p3 --label=system -f
+mkfs.fat -F 32 -n EFI /dev/"$dev"p1
+mkswap -L swap -f /dev/"$dev"p2
+mkfs.btrfs /dev/"$dev"p3 --label=system -f
 o=defaults,x-mount.mkdir
 o_btrfs=$o,defaults,noatime,compress=zstd,commit=120
 mount -t btrfs LABEL=system /mnt 
@@ -55,7 +59,7 @@ mount -t btrfs -o subvol=@tmp,$o_btrfs LABEL=system /mnt/var/tmp
 mount -t btrfs -o subvol=@cache,$o_btrfs LABEL=system /mnt/var/cache
 mkdir /mnt/boot
 mkdir /mnt/boot/efi
-mount /dev/nvme0n1p1 /mnt/boot
-swapon /dev/nvme0n1p2
+mount /dev/"$dev"p1 /mnt/boot
+swapon /dev/"$dev"p2
 btrfs quota enable /mnt
 lsblk
